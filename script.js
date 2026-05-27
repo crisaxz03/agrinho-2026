9629ff28988d4e5deeaaed6fc4eae2c6134691c8
crisaxz03 <cristhoffer.tavares@escola.pr.gov.br>
2026-05-27 20:53:15 -0300

    teste

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Aguarda o documento HTML carregar completamente
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Seleciona o botão através do ID
+    const botaoSaibaMais = document.getElementById("btnSaibaMais");
 
+    // Adiciona um evento de clique ao botão
+    botaoSaibaMais.addEventListener("click", function() {
+        // Faz a página rolar suavemente até a seção de práticas
+        document.getElementById("praticas").scrollIntoView({ 
+            behavior: "smooth" 
+        });
+    });
+});
